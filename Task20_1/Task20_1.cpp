80c3ceef464739bfd6af42d801368e05e6bbf087
DmityFilchev <filchev_dmitry@mail.ru>
2022-06-29 14:08:02 +0300

    Добавьте файлы проекта.

---
+++ Task20_1/Task20_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+
+int main()
+{
+    std::string date;
+    std::string name;
+    int sum = 0;
+    std::cout << "Enter a name : ";
+    std::getline(std::cin, name);
+    bool ok;
+    do
+    {
+        std::cout << "Date : ";
+        std::cin >> date;
+        int a = stoi(date.substr(0, 2));
+        int b = stoi(date.substr(3, 2));
+        int c = stoi(date.substr(6, 4));
+        if (a > 31 || b > 12 || c>2022)
+        {
+            std::cout << "Error!!!!\n";
+            ok = true;
+        }
+        else {
+            ok = false;
+        }
+    } while (ok);
+    std::cout << "Sum :";
+    std::cin >> sum;
+    std::ofstream text("statement.txt",std::ios::app);
+    text <<name<<" "<<date<<" "<<sum<< std::endl;
+    text.close();
+}
